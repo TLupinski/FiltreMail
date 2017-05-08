95d4fe67b71eebd3f5814e4c97bea10f5dc9ebca
Thibault Lupinski <ts.lupinski@gmail.com>
2017-05-08 14:01:00 +0200

    Correction de quelques erreurs, ajout du rapport

--- FiltreAntiSpam.java
+++ FiltreAntiSpam.java
@@ -29,43 +29,36 @@ public class FiltreAntiSpam {
 
 	private ArrayList<String> dictionnaire;
 
-	public FiltreAntiSpam() {
+	public FiltreAntiSpam() throws Exception {
 		charger_dictionnaire();
 	}
 
-	public FiltreAntiSpam(String fichier) {
+	public FiltreAntiSpam(String fichier) throws Exception {
 		charger_dictionnaire(fichier);
 	}
 
-	public void charger_dictionnaire(){
+	public void charger_dictionnaire() throws Exception{
 		charger_dictionnaire("./res/dictionnaire1000en.txt");
 	}
 	
-	public void charger_dictionnaire(String fichier){
+	public void charger_dictionnaire(String fichier) throws Exception {
 		dictionnaire = new ArrayList<>();
 		String ligne;
 		System.out.println("Chargement du dictionnaire : "+fichier);
-		try{
-			InputStream is=new FileInputStream(fichier); 
-			InputStreamReader isr=new InputStreamReader(is);
-			BufferedReader br=new BufferedReader(isr);
-			
-			while ((ligne=br.readLine())!=null){	
-				// On ne charge que les mots d'au moins trois lettres, ceux de deux sont généralement des pronoms peu impactant sur le classement car omniprésents.
-				if(ligne.length() >= 3){
-					dictionnaire.add(ligne);
-				}
+		InputStream is=new FileInputStream(fichier); 
+		InputStreamReader isr=new InputStreamReader(is);
+		BufferedReader br=new BufferedReader(isr);	
+		while ((ligne=br.readLine())!=null){	
+			// On ne charge que les mots d'au moins trois lettres, ceux de deux sont généralement des pronoms peu impactant sur le classement car omniprésents.
+			if(ligne.length() >= 3){
+				dictionnaire.add(ligne);
 			}
-			br.close(); 
-		}		
-		catch (Exception e){
-			e.printStackTrace();
-			System.out.println("Echec du chargement!");
 		}
+		br.close(); 
 		System.out.println("Chargement du dictionnaire terminé !");
 	}
 	
-	public boolean[] vecteur_occurence(String fichier){
+	public boolean[] vecteur_occurence(String fichier) throws Exception {
 		int taille = dictionnaire.size();
 		boolean mots[] = new boolean[taille];
 		for(int i = 0; i < mots.length;i++){
@@ -76,79 +69,65 @@ public class FiltreAntiSpam {
 		int index;
 		String sac[];
 		String regex = "[^A-Za-z]";
-		try{
-			InputStream is=new FileInputStream(fichier); 
-			InputStreamReader isr=new InputStreamReader(is);
-			BufferedReader br=new BufferedReader(isr);
-			
-			while ((ligne=br.readLine())!=null){
-				sac = ligne.split(regex);
-				for(String mot : sac){
-					//On supprime les mots de moins de 3 lettres, les pronoms ne sont pas intéressants.
-					if(mot.length()>=3){
-						mot = mot.toUpperCase();
-						if(dictionnaire.contains(mot)){
-							index = dictionnaire.indexOf(mot);
-							mots[index] = true;
-						}
+		InputStream is=new FileInputStream(fichier); 
+		InputStreamReader isr=new InputStreamReader(is);
+		BufferedReader br=new BufferedReader(isr);
+		
+		while ((ligne=br.readLine())!=null){
+			sac = ligne.split(regex);
+			for(String mot : sac){
+				//On supprime les mots de moins de 3 lettres, les pronoms ne sont pas intéressants.
+				if(mot.length()>=3){
+					mot = mot.toUpperCase();
+					if(dictionnaire.contains(mot)){
+						index = dictionnaire.indexOf(mot);
+						mots[index] = true;
 					}
 				}
 			}
-			br.close(); 
-		}		
-		catch (Exception e){
-			e.printStackTrace();
 		}
-		
+		br.close(); 
 		return mots;
 	}
 	
-	public void charger_apprentissage() {
+	public void charger_apprentissage() throws Exception {
 		charger_apprentissage("./res/mon_classifieur.txt");
 	}
 	
-	public void charger_apprentissage(String classifieurPath) {
+	public void charger_apprentissage(String classifieurPath) throws Exception {
 		System.out.println("Chargement de la base d'apprentissage : "+classifieurPath);
-		try 
+		int taille = dictionnaire.size();
+		InputStream is=new FileInputStream(classifieurPath); 
+		InputStreamReader isr=new InputStreamReader(is);
+		BufferedReader br=new BufferedReader(isr);
+		String line, mots[];
+		String regex = "\\|";
+		line = br.readLine();
+		mots = line.split(regex);
+		NMail = Integer.parseInt(mots[0]);
+		NSpam = Integer.parseInt(mots[1]);
+		NHam = Integer.parseInt(mots[2]);
+		PSpam = (double) ( (double) NSpam / (double) NMail) ;
+		PHam = (double) ( (double) NHam / (double) NMail) ;
+		nbSpam = new int[taille];
+		nbHam = new int[taille];
+		bSpam = new double[taille];
+		bHam = new double[taille];
+		line = br.readLine();
+		mots = line.split(regex);
+		for (int i = 0; i < taille; i++)
 		{
-			int taille = dictionnaire.size();
-			InputStream is=new FileInputStream(classifieurPath+".txt"); 
-			InputStreamReader isr=new InputStreamReader(is);
-			BufferedReader br=new BufferedReader(isr);
-			String line, mots[];
-			String regex = "\\|";
-			line = br.readLine();
-			mots = line.split(regex);
-			NMail = Integer.parseInt(mots[0]);
-			NSpam = Integer.parseInt(mots[1]);
-			NHam = Integer.parseInt(mots[2]);
-			PSpam = (double) ( (double) NSpam / (double) NMail) ;
-			PHam = (double) ( (double) NHam / (double) NMail) ;
-			nbSpam = new int[taille];
-			nbHam = new int[taille];
-			bSpam = new double[taille];
-			bHam = new double[taille];
-			line = br.readLine();
-			mots = line.split(regex);
-			for (int i = 0; i < taille; i++)
-			{
-				nbSpam[i] = Integer.parseInt(mots[i]);
-				bSpam[i] = (double) ( (double) (bSpam[i] + epsilon) / (double) (NSpam + 2*epsilon) );
-			}
-			line = br.readLine();
-			mots = line.split(regex);
-			for (int i = 0; i < taille; i++)
-			{
-				nbHam[i] = Integer.parseInt(mots[i]);
-				bHam[i] = (double) ( (double) (bSpam[i] + epsilon) / (double) (NHam + 2*epsilon) );
-			}
-			br.close();
-			
-		} catch (Exception e)
-		{
-			e.printStackTrace();
-			System.out.println("Echec du chargement de la base d'apprentissage!");
+			nbSpam[i] = Integer.parseInt(mots[i]);
+			bSpam[i] = (double) ( (double) (nbSpam[i] + epsilon) / (double) (NSpam + 2*epsilon) );
 		}
+		line = br.readLine();
+		mots = line.split(regex);
+		for (int i = 0; i < taille; i++)
+		{
+			nbHam[i] = Integer.parseInt(mots[i]);
+			bHam[i] = (double) ( (double) (nbHam[i] + epsilon) / (double) (NHam + 2*epsilon) );
+		}
+		br.close();
 		System.out.println("Chargement de la base d'apprentissage effectué avec succès !");
 	}
 	
@@ -177,15 +156,14 @@ public class FiltreAntiSpam {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		boolean b = this.afficher();
-		System.out.println(b);
+		System.out.println("Yolololo");
 	}
 
-	public void apprentissage_mails(int nbSpams,int nbHams) {
+	public void apprentissage_mails(int nbSpams,int nbHams) throws Exception {
 		apprentissage_mails(nbSpams,nbHams,"./res/baseapp");
 	}
 	
-	public void apprentissage_mails(int nbSpams, int nbHams, String repertoire) {
+	public void apprentissage_mails(int nbSpams, int nbHams, String repertoire) throws Exception {
 		if (nbSpams <= 0)
 			nbSpams = 200;
 		if (nbHams <= 0)
@@ -201,59 +179,54 @@ public class FiltreAntiSpam {
 		
 		int[] apparitionMotsSpam= new int[3];
 		int[] apparitionMotsHam = new int[3];
-		try {
-			apparitionMotsSpam = apprentissage_occurrence_mots_mails(SpamDirectory, NSpam);
-			//System.out.print(".");
-			apparitionMotsHam = apprentissage_occurrence_mots_mails(HamDirectory, NHam); 
-			//System.out.print(".");
-			
-
-			int taille = dictionnaire.size();
-			nbSpam = new int[taille];
-			nbHam = new int[taille];
-			bSpam = new double[taille];
-			bHam = new double[taille];
-			
-			//ESTIMATION DES PARAMETRES AVEC LISSAGE
-			for(int i=0; i<taille; i++){
-				this.nbSpam[i] = apparitionMotsSpam[i];
-				this.nbHam[i] = apparitionMotsHam[i];
-				this.bSpam[i] = (double) ( (double) (bSpam[i] + epsilon) / (double) (NSpam + 2*epsilon) );
-				this.bHam[i] = (double) ( (double) (bSpam[i] + epsilon) / (double) (NHam + 2*epsilon) );
-			}
-			
-			//CALCULE PROBABILITE A POSTERIORI
-			this.PSpam = (double) ( (double) NSpam / (double) NMail) ;
-			this.PHam = (double) ( (double) NHam / (double) NMail) ;
-			if (PHam + PSpam != 1)
-			{
-				System.out.println("\nErreur dans les probabilités Pham +PSpam = "+(PHam+PSpam)+"\n");
-			}
-			System.out.println("PHam : "+PHam+"\t"+"PSpam : "+PSpam);
-			
-			System.out.println("Apprentissage terminé !");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+		apparitionMotsSpam = apprentissage_occurrence_mots_mails(SpamDirectory, NSpam);
+		//System.out.print(".");
+		apparitionMotsHam = apprentissage_occurrence_mots_mails(HamDirectory, NHam); 			//System.out.print(".");
+		
+		int taille = dictionnaire.size();
+		nbSpam = new int[taille];
+		nbHam = new int[taille];
+		bSpam = new double[taille];
+		bHam = new double[taille];
+		
+		//ESTIMATION DES PARAMETRES AVEC LISSAGE
+		for(int i=0; i<taille; i++){
+			this.nbSpam[i] = apparitionMotsSpam[i];
+			this.nbHam[i] = apparitionMotsHam[i];
+			this.bSpam[i] = (double) ( (double) (bSpam[i] + epsilon) / (double) (NSpam + 2*epsilon) );
+			this.bHam[i] = (double) ( (double) (bSpam[i] + epsilon) / (double) (NHam + 2*epsilon) );
 		}
-
+			
+		//CALCULE PROBABILITE A POSTERIORI
+		this.PSpam = (double) ( (double) NSpam / (double) NMail) ;
+		this.PHam = (double) ( (double) NHam / (double) NMail) ;
+		if (PHam + PSpam != 1)	
+		{
+			System.out.println("\nErreur dans les probabilités Pham +PSpam = "+(PHam+PSpam)+"\n");
+			
+		}
+		System.out.println("PHam : "+PHam+"\t"+"PSpam : "+PSpam);
+			
+	System.out.println("Apprentissage terminé !");
 	}
 
-	public void apprentissage_mail(String mailPath, int type) {
+	public void apprentissage_mail(String mailPath, int type) throws Exception {
 		boolean[] vecteur = this.vecteur_occurence(mailPath);
 		double probaSpam=0, probaHam=0;
-		
+
+		if (type == 0)
+			NHam += 1;
+		else
+			NSpam += 1;
 		for(int i = 0; i < vecteur.length; i++){
 			int v = vecteur[i] ? 1 : 0;
 			if (type == 0){
 				nbHam[i] += v;
-				NHam++;
 				bHam[i] = (nbHam[i]+epsilon)/(NHam+2*epsilon);
 			}
 			else {
 				nbSpam[i] += v;
-				NSpam++;
-				bSpam[i] = (bSpam[i]*NSpam+v+epsilon)/(NSpam+1+2*epsilon);
+				bSpam[i] = (nbSpam[i]+epsilon)/(NSpam+2*epsilon);
 			}
 		}
 	}
@@ -285,7 +258,7 @@ public class FiltreAntiSpam {
 	/*	Fonction qui prend en paramètre l'adresse d'un fichier mail à lire et qui retourne si il a plus de chance d'être un spam ou un ham
 	 * 	Retourne 0 si c'est un HAM, 1 si c'est un SPAM, -1 en cas d'erreur.
 	 */
-	public int evaluer_mail(String mailPath) {
+	public int evaluer_mail(String mailPath) throws Exception {
 		boolean[] vecteur = this.vecteur_occurence(mailPath);
 		double probaSpam=0, probaHam=0;
 		
@@ -308,94 +281,28 @@ public class FiltreAntiSpam {
 		double px = -Math.log(Math.pow(0.5,dictionnaire.size()));
 		probaSpam += px;
 		probaHam += px;
-		double pSpam = 1.0 / (1.0 + Math.exp(probaHam - probaSpam));
+		double pSpam = 1.0 / (1.0 + Math.exp(probaSpam - probaHam));
 		double pHam = 1.0 / (1.0 + Math.exp(probaSpam - probaHam));
 		
 		System.out.print(": P(Y=SPAM | X=x) =" + pSpam + ", P(Y=HAM | X=x) =" + pHam);
 		
-		if (probaSpam > probaHam)
-			return 1;
-		return 0;
-	}
-	
-	public int evaluer_mail(String mailPath, int type) {
-		boolean[] vecteur = this.vecteur_occurence(mailPath);
-		double probaSpam=0, probaHam=0;
-		
-		for(int i = 0; i < vecteur.length; i++)
-		{
-			int v = 0;
-			if (vecteur[i])
-			{
-				probaSpam += Math.log(bSpam[i]);
-				probaHam += Math.log(bHam[i]);
-				v = 1;
-			} else {
-				probaSpam += Math.log(1-bSpam[i]);
-				probaHam += Math.log(1-bHam[i]);
-			}
-		}
-		if (PSpam > 0)
-			probaSpam += Math.log(PSpam);
-		if(PHam > 0)
-			probaHam += Math.log(PHam);
-		
-		double px = Math.log(1 / Math.pow(0.5,dictionnaire.size()));
-		probaSpam += px;
-		probaHam += px;
-		double pSpam = 1.0 / (1.0 + Math.exp(probaHam));
-		double pHam = 1.0 / (1.0 + Math.exp(probaSpam));
-		
-		System.out.print(": P(Y=SPAM | X=x) =" + pSpam + ", P(Y=HAM | X=x) =" + pHam);
-
-		this.PSpam = (double) ( (double) NSpam / (double) NMail) ;
-		this.PHam = (double) ( (double) NHam / (double) NMail) ;
-		//sauvegarder_apprentissage();
-		
 		if (pSpam > pHam)
 			return 1;
 		return 0;
-		/*
-		PMailSpam=Math.log10(PMailSpam);
-		PMailHam=Math.log10(PMailHam);
-		evidence=PMailHam+PMailSpam;
-		PMailSpam=PMailSpam/evidence;
-		PMailHam=PMailHam/evidence;
-		
-		
-		System.out.println(": P(Y=SPAM | X=x) =" + PMailSpam + ", P(Y=HAM | X=x) =" + PMailHam);
-		System.out.print("              =>");
-
-		// Estimation SPAM ou HAM
-		double res = Math.max(PMailSpam, PMailHam);
-		if(res == PMailHam){
-			// mail considere comme un HAM
-			return false;
-		} else if(res == PMailSpam){
-			// mail considere comme un SPAM
-			return true;
-		} else {
-			throw new Exception("Critical Error");
-		}
-		 */
-	}
-	public void lancer_test(String directoryPath, String mailName)
-	{
-		File mail = new File(directoryPath+"/"+mailName);
 	}
 	
-	public void lancer_tests() {
-		lancer_tests("./res/basetest");
+	public void lancer_tests(int nbSpams, int nbHams) throws Exception {
+		lancer_tests(nbSpams, nbHams, "./res/basetest");
 	}
 	
-	public void lancer_tests(String directoryName) {
+	public void lancer_tests(int nbSpams, int nbHams, String directoryName) throws Exception {
 		int nbTrueSpam = 0, nbSpam, nbTrueHam = 0, nbHam;
 		String files[], resultat[] = {"HAM", "SPAM", "ERREUR"};
 		//Test sur les SPAMs
 		String testDirectoryName = directoryName+"/spam";
 		File repertoire = new File(testDirectoryName);
 		files= repertoire.list();
-		nbSpam = files.length;
+		nbSpam = Math.min(files.length,nbSpams);
 		int resultatAttendu = 1;
 		for (int i = 0; i < nbSpam; i++)
 		{
@@ -411,13 +318,13 @@ public class FiltreAntiSpam {
 		testDirectoryName = directoryName+"/ham";
 		repertoire = new File(testDirectoryName);
 		files= repertoire.list();
-		nbHam = files.length;
+		nbHam = Math.min(files.length,nbHams);
 		resultatAttendu = 0;
 		
-		for (int i = 0; i < nbSpam; i++)
+		for (int i = 0; i < nbHam; i++)
 		{
 			System.out.print("\n"+resultat[resultatAttendu]+" numero "+i+" ");
-			int res = evaluer_mail(testDirectoryName+"/"+files[i],resultatAttendu);
+			int res = evaluer_mail(testDirectoryName+"/"+files[i]);
 			System.out.print("\n          => identifié comme un "+resultat[res]);
 			if (res != resultatAttendu){
 				nbTrueHam++;
@@ -440,90 +347,95 @@ public class FiltreAntiSpam {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		FiltreAntiSpam fas = new FiltreAntiSpam("./res/dictionnaire1000en.txt");
-		StringBuilder sb = new StringBuilder();
-		sb.append("Nombre d'arguments insuffisants\n");
-		sb.append("Pour commencer l'apprentissage :\n      FiltreAntiSpam apprend_filtre [@DossierMailsApprentissage] [NbSpams] [NbHams] [@Classifeur]\n");
-		sb.append("Pour continuer l'apprentissage :\n      FiltreAntiSpam apprend_filtre_enligne @Mail TypeMail [@Classifieur] \n");
-		sb.append("Pour tester des mails :\n      FiltreAntiSpam filtre_mail [@Classifieur] [@DossierMailsTest] [NomDuMail]\n");
-		String repertoire, nom, classifieur;
-		int nbSpams = 200, nbHams = 200, type;
-		String help = sb.toString();
-		if (args.length <= 0){
-			System.out.println(help);
-		}
-		switch (args[0])
-		{
-			case "apprend_filtre":
-				if (args.length >= 3)
-					nbSpams = Integer.parseInt(args[2]);
-				if (args.length >= 4)
-					nbSpams = Integer.parseInt(args[3]);
-				if (args.length < 2)
-					fas.apprentissage_mails(nbSpams,nbHams);
-				else{
-					repertoire = args[1];
-					fas.apprentissage_mails(nbSpams,nbHams,repertoire);
-				}
-				if (args.length >= 5)
-					fas.sauvegarder_apprentissage();
-				else {
-					classifieur = args[4];
-					fas.sauvegarder_apprentissage(classifieur);
-				}
-				break;
-			case "apprend_filtre_enligne":
-				if (args.length < 3)
-					System.out.println(help);
-				else {
-					nom = args[1];
-					if (args[2] == "SPAM")
-						type = 1;
-					else if (args[2] == "HAM")
-						type = 0;
-					else{
-						System.out.println("Erreur, "+args[2]+" n'est pas un type valide. Seul SPAM et HAM sont des types valides");
-						break;
-					}
-					nom = args[3];
-					if (args.length < 4){
-						fas.charger_apprentissage();
-						fas.apprentissage_mail(nom, type);
-						fas.sauvegarder_apprentissage();
-						
-					}
-					else{
-						classifieur = args[4];
-						fas.charger_apprentissage(classifieur);
-						fas.apprentissage_mail(nom, type);
-						fas.sauvegarder_apprentissage(classifieur);
-					}
-				}
-				break;
-			case "filtre_mail":
-				if (args.length < 2)
-					fas.charger_apprentissage();
-				else {
-					classifieur = args[1];
-					fas.charger_apprentissage(classifieur);
-				}
-				if (args.length < 3)
-					repertoire = "./basetest";
-				else
-					repertoire = args[2];
-				if (args.length < 4)
-					fas.lancer_tests(repertoire);
-				else{
-					nom = args[4];
-					fas.lancer_test(repertoire, nom);
-				}
-				break;
-			case "help":
+		try {
+			FiltreAntiSpam fas = new FiltreAntiSpam("./res/dictionnaire1000en.txt");
+			StringBuilder sb = new StringBuilder();
+			sb.append("Nombre d'arguments insuffisants\n");
+			sb.append("Pour commencer l'apprentissage :\n      FiltreAntiSpam apprend_filtre [@DossierMailsApprentissage] [NbSpams] [NbHams] [@Classifeur]\n");
+			sb.append("Pour continuer l'apprentissage :\n      FiltreAntiSpam apprend_filtre_enligne @Mail TypeMail [@Classifieur] \n");
+			sb.append("Pour tester des mails :\n      FiltreAntiSpam filtre_mail [NbSpams] [NbHams] [@Classifieur] [@DossierMailsTest] \n");
+			String repertoire, nom, classifieur;
+			int nbSpams = 200, nbHams = 200, type;
+			String help = sb.toString();
+			if (args.length <= 0){
 				System.out.println(help);
-				break;
-			default:
-				System.out.println("Opération inconnue. Pour accéder à l'aide, FiltreAntiSpam help");
-				break;
+			} else 
+			{
+				switch (args[0])
+				{
+					case "apprend_filtre":
+						if (args.length >= 3)
+							nbSpams = Integer.parseInt(args[2]);
+						if (args.length >= 4)
+							nbHams = Integer.parseInt(args[3]);
+						if (args.length < 2)
+							fas.apprentissage_mails(nbSpams,nbHams);
+						else{
+							repertoire = args[1];
+							fas.apprentissage_mails(nbSpams,nbHams,repertoire);
+						}
+						if (args.length < 5)
+							fas.sauvegarder_apprentissage();
+						else {
+							classifieur = args[4];
+							fas.sauvegarder_apprentissage(classifieur);
+						}
+						break;
+					case "apprend_filtre_enligne":
+						if (args.length < 3)
+							System.out.println(help);
+						else {
+							nom = args[1];
+							if (args[2].equals("SPAM"))
+								type = 1;
+							else if (args[2].equals("HAM"))
+								type = 0;
+							else{
+								System.out.println("Erreur, "+args[2]+" n'est pas un type valide. Seul SPAM et HAM sont des types valides");
+								break;
+							}
+							if (args.length < 4){
+								fas.charger_apprentissage();
+								fas.apprentissage_mail(nom, type);
+								fas.sauvegarder_apprentissage();
+								
+							}
+							else{
+								classifieur = args[3];
+								fas.charger_apprentissage(classifieur);
+								fas.apprentissage_mail(nom, type);
+								fas.sauvegarder_apprentissage(classifieur);
+							}
+						}
+						break;
+					case "filtre_mail":
+						if (args.length >= 2)
+							nbSpams = Integer.parseInt(args[1]);
+						if (args.length >= 3)
+							nbHams = Integer.parseInt(args[2]);
+						if (args.length < 4)
+							fas.charger_apprentissage();
+						else {
+							classifieur = args[3];
+							fas.charger_apprentissage(classifieur);
+						}
+						if (args.length < 5)
+							fas.lancer_tests(nbSpams, nbHams);
+						else{
+							repertoire = args[4];
+							fas.lancer_tests(nbSpams, nbHams, repertoire);
+						}
+						break;
+					case "help":
+						System.out.println(help);
+						break;
+					default:
+						System.out.println("Opération inconnue. Pour accéder à l'aide, FiltreAntiSpam help");
+						break;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
